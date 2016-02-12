78ee358dbbc7238caf59cdb133ef9f7d8733155e
Joe Schafer <joe@jschaf.com>
2016-02-12 12:00:35 -0500

    refactor: reformat everything

--- src/test/groovy/tododrop/TodoResourceTest.groovy
+++ src/test/groovy/tododrop/TodoResourceTest.groovy
@@ -23,49 +23,46 @@ class TodoResourceTest extends Specification {
 
     def "Get root with no Todos returns empty list"() {
         given: "no objects in todoStore"
-            todoStore.getAll() >> []
+        todoStore.getAll() >> []
 
         when: "we GET root"
-            def result = resources.client().target("/").request().get(List)
+        def result = resources.client().target("/").request().get(List)
 
         then: "we receive an empty list"
-            result == []
+        result == []
     }
 
     def "Get root with 1 Todo returns 1 Todo"() {
         given: "one Todo in TodoStore"
-            Todo todo = new Todo(1, "title", null, null, null)
-            todoStore.getAll() >> [todo]
+        Todo todo = new Todo(1, "title", null, null, null)
+        todoStore.getAll() >> [todo]
 
         when: "we GET root"
-            List<Todo> result = resources.client().target("/").request().get(List)
+        List<Todo> result = resources.client().target("/").request().get(List)
 
         then: "we get a list of one todo"
-            result.size() == 1
-            result.get(0).id == todo.id
-            result.get(0).title == todo.title
+        result.size() == 1
+        result.get(0).id == todo.id
+        result.get(0).title == todo.title
     }
 
     def "Adding a todo increases number of Todos"() {
         given: "no todos in TodoStore"
-            Todo todo = new Todo(1, "title", null, null, null)
-            todoStore.save(_ as Todo) >> todo
+        Todo todo = new Todo(1, "title", null, null, null)
+        todoStore.save(_ as Todo) >> todo
+
         when: "we add a Todo"
-            def response = resources.client().target("/")
+        def response = resources.client().target("/")
                 .request(APPLICATION_JSON_TYPE)
                 .post(entity(todo, APPLICATION_JSON_TYPE))
+
         then:
-            // How do you test that the returned Todo is the same?
+        // How do you test that the returned Todo is the same?
 
-            // FAILS: Why doesn't this pass, it's returning 204 - no content
-//            response.getStatusInfo() == Response.Status.OK
+        // FAILS: Why doesn't this pass, it's returning 204 - no content
+        response.getStatusInfo() == Response.Status.OK
 
-            1 * todoStore.save(_ as Todo)
+        1 * todoStore.save(_ as Todo)
     }
-
-
-
-
-
 }
 
