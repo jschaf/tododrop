602386d377b662dac4a981f05963146e95fe2e9e
Joe Schafer <joe@jschaf.com>
2016-02-14 18:33:21 -0500

    feat: omg it works!

--- setup_docker_env.sh
+++ setup_docker_env.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
 # We need to translate the environmental variables provided by the docker
-# linking into usable variables.  The prefix "DB" (in $DB_ENV_DB_USER for
-# example) is determined by the links argument in the gradle task
-# createJavaAppContainer.  The rest of the environmental variable was defined
-# by the base image we used for the postgresql database defined in the gradle
-# task createPostgresDockerfile
+# linking into usable variables.  The prefix DB_ENV used below is determined
+# by the links argument in the gradle task createJavaAppContainer.  For example:
+#
+# links = ["original_container_name:alias"]
+#
+# Would give us environmental variable names like ALIAS_ENV_USER. The part of
+# the environmental variable after ALIAS_ENV was defined by the base image we used
+# for the postgresql database.  The base image is defined in the gradle task
+# buildPostgresDockerfile.
 export DATABASE_USER="$DB_ENV_DB_USER"
 export DATABASE_PASSWORD="$DB_ENV_DB_PASS"
 export JDBC_DATABASE_URL="jdbc:postgresql://$DB_PORT_5432_TCP_ADDR:$DB_PORT_5432_TCP_PORT/$DB_ENV_DB_NAME"
 
-# Starting the dropwizard app
-echo "PWD IS: $(pwd)"
-echo "FILES ARE: $(ls)"
+# Start the dropwizard app
 java -jar tododrop-0.1-all.jar server tododrop.yml
